dd962806c9755c35d03f8587ead66f9fc1fbd522
Firdaus Dharma <firdausdharma@gmail.com>
2023-04-04 19:40:12 +0700

    Upload File

--- Skincare_Review/2/app_selectbox.py
+++ Skincare_Review/2/app_selectbox.py
@@ -5,27 +5,30 @@ from itertools import filterfalse
 st.title('Analisis Produk Skincare Berdasarkan Review Pelanggan Menggunakan Algoritma Apriori')
 
 
-df = pd.read_csv('Skincare_Review/2/nyka_top_brands_cosmetics_product_reviews.csv',sep=',')
+df = pd.read_csv(
+    'Skincare_Review/2/nyka_top_brands_cosmetics_product_reviews.csv', sep=',')
 
-df = df[['review_date','author','brand_name','product_title','price','review_rating','product_rating']]
+df = df[['review_date', 'author', 'brand_name', 'product_title','price', 'review_rating', 'product_rating']]
 
 # membuat pilihan yang tersedia
-options = ['Olay', 'Nykaa Naturals', 'Nykaa Cosmetics', 'Nivea', 'NYX Professional Makeup', 'Maybelline New York', 'Lakme', "L'Oreal Paris", 'Kay Beauty', 'Herbal Essences']
+options = ['Olay', 'Nykaa Naturals', 'Nykaa Cosmetics', 'Nivea', 'NYX Professional Makeup','Maybelline New York', 'Lakme', "L'Oreal Paris", 'Kay Beauty', 'Herbal Essences']
 
 # membuat multiselect dengan label "Select your favorite brands"
 st.write("---")
-selected_options = st.multiselect('Select Brand Name :', sorted(options))
+selected_options = st.multiselect('Filter Brand Name :', sorted(options))
 
-if len(selected_options) != 0 :
+if len(selected_options) != 0:
     df = df.loc[df['brand_name'].isin(selected_options)]
 else:
     df = df
 
-rule = pd.read_csv('Skincare_Review/2/result_rules.csv',sep=',')
+rule = pd.read_csv('Skincare_Review/2/result_rules.csv', sep=',')
 
 # Mengganti tanda kurung pada kolom 'items'
-rule['antecedents'] = rule['antecedents'].apply(lambda x: [item.strip() for item in x.strip('()').split(',')])
-rule['consequents'] = rule['consequents'].apply(lambda x: [item.strip() for item in x.strip('()').split(',') if item.strip() != ''])
+rule['antecedents'] = rule['antecedents'].apply(
+    lambda x: [item.strip() for item in x.strip('()').split(',')])
+rule['consequents'] = rule['consequents'].apply(
+    lambda x: [item.strip() for item in x.strip('()').split(',') if item.strip() != ''])
 
 product = set(df["product_title"].values)
 
@@ -39,10 +42,10 @@ product.insert(0, None)
 
 user_input = []
 
-user_input.append(st.selectbox('Item 1',product,key='item1'))
-user_input.append(st.selectbox('Item 2',product,key='item2'))
-user_input.append(st.selectbox('Item 3',product,key='item3'))
-user_input.append(st.selectbox('Item 4',product,key='item4'))
+user_input.append(st.selectbox('Item 1', product, key='item1'))
+user_input.append(st.selectbox('Item 2', product, key='item2'))
+user_input.append(st.selectbox('Item 3', product, key='item3'))
+user_input.append(st.selectbox('Item 4', product, key='item4'))
 
 # user_input.append(st.text_input('Item 1',key='item1'))
 # user_input.append(st.text_input('Item 2',key='item2'))
@@ -59,39 +62,39 @@ antecedents = rule['antecedents'].values
 consequents = rule['consequents'].values
 
 a = []
-for i,v in enumerate(antecedents):
+for i, v in enumerate(antecedents):
     a.append(sorted(antecedents[i]))
 
 b = []
-for i,v in enumerate(a):
+for i, v in enumerate(a):
     b.append([x for x in a[i] if x != ''])
 
-for i,v in enumerate(b):
+for i, v in enumerate(b):
     c = [[s.replace("'", "") for s in inner] for inner in b]
 
 d = []
-for i,v in enumerate(c):
+for i, v in enumerate(c):
     d.append(sorted(c[i]))
 
 # batas
 
 e = []
-for i,v in enumerate(consequents):
+for i, v in enumerate(consequents):
     e.append(sorted(consequents[i]))
 
 f = []
-for i,v in enumerate(e):
+for i, v in enumerate(e):
     f.append([x for x in e[i] if x != ''])
 
-for i,v in enumerate(f):
+for i, v in enumerate(f):
     g = [[s.replace("'", "") for s in inner] for inner in f]
 
 
 h = []
-for i,v in enumerate(g):
+for i, v in enumerate(g):
     h.append((g[i]))
 
-#batas
+# batas
 output = pd.DataFrame()
 output['antecedents'] = c
 output['consequents'] = g
@@ -115,9 +118,6 @@ else:
     st.error('Silahkan Input Item!')
 
 
-
-
-
 # # daftar opsi yang akan ditampilkan pada Selectbox
 # options = ['option 1', 'option 2', 'option 3', 'option 4', 'option 5']
 
@@ -129,4 +129,3 @@ else:
 
 # # menampilkan Selectbox dengan opsi yang sudah disaring
 # selected_option = st.selectbox('Select an option', options=filtered_options, multiple=True)
-
